c2cff7a26e258b2a48db2deeeddb9aaba9294ef3
TimLee <xodnjs479@gmail.com>
2022-01-26 04:56:58 +0900

    sql 쿼리문 추가

--- login_ok.php
+++ login_ok.php
@@ -5,7 +5,7 @@
     $userid = $_POST['userid'];
     $userpw = $_POST['userpw'];
 
-    $sql = "select useridx, userid, username from tb_user where userid='$userid' and userpw=md5('$userpw')";
+    $sql = "select useridx, userid, username, userphone, useremail, userhobby from tb_user where userid='$userid' and userpw=md5('$userpw')";
     $result = mysqli_query($conn, $sql);
     $data = mysqli_fetch_array($result);
 
@@ -15,7 +15,10 @@
         $_SESSION['userid'] = $userid;
         $_SESSION['useridx'] = $data['useridx'];
         $_SESSION['username'] = $data['username'];
-        echo "<script>alert('Login Success!'); location.href='main.php';</script>";
+        $_SESSION['useremail'] = $data['useremail'];
+        $_SESSION['userphone'] = $data['userphone'];
+        $_SESSION['userhobby'] = $data['userhobby'];
+        echo "<script>alert('Login Success! Welcome {$data['username']}'); location.href='main.php';</script>";
     } else {
         echo "<script>alert('Login Failed! Check your ID & Password.'); history.back();</script>";
     }
